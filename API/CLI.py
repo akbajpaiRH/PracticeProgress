978296015419c71de6f68083df1ad1b6c88b5758
akbajpaiRH <akbajpai@redhat.com>
2022-12-15 00:21:28 +0530

    subprocess CLI Testing

--- API/CLI.py
+++ API/CLI.py
@@ -1,6 +1,25 @@
 import subprocess
+import os
+
+print(os.getcwd())
+
+os.chdir(os.getcwd())
 
 s = subprocess.check_output(["pytest", "-v"], encoding='utf-8')
-assert "2 passed" in s
-assert "failed" not in s
+# s2 = subprocess.check_output(["ls"], encoding='utf-8')
 print(s)
+num = 0
+temp = s.split("\n")
+for line in temp:
+    if line.startswith("collecting"):
+        # print(line)
+        num = line.split(" ")[3]
+        # print(num)
+        # print(type(num))
+
+assert "platform linux" in s
+assert "Python 3.9.13" in s
+assert "pytest-7.2.0" in s
+assert "pluggy-1.0.0" in s
+assert num + " passed" in s
+assert "failed" not in s
