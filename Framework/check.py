3eac12d947c59861e965bda721dd93250cd3982b
akbajpaiRH <akbajpai@redhat.com>
2023-01-14 17:11:57 +0530

    Framework [pytest+subprocess]

--- Framework/check.py
+++ Framework/check.py
@@ -1,28 +1,30 @@
 import subprocess
 import os
+import time
+
 
 class Test:
     def create(self,str):
         # print(os.getcwd())
         # print(str)
-        cmd = 'touch Subprocess/DEMO/{file_name}'
+        cmd = 'touch /home/akbajpai/PycharmProjects/pythonProject/Subprocess/DEMO/{file_name}'
         cmd = cmd.format(file_name=str)
         print(cmd)
         p1=subprocess.Popen([cmd], shell=True, encoding='utf-8')
         # p1=subprocess.Popen(['touch','./Subprocess/DEMO/',str],encoding='utf-8')
-    def check(self,str):
+    def check(self,st):
         # print(os.getcwd())
         p1 = subprocess.check_output(['ls', '/home/akbajpai/PycharmProjects/pythonProject/Subprocess/DEMO/'],
                                      encoding='utf-8')
-        if str in p1:
-            print("-------------FOUND------------------"+str)
-            subprocess.call(['rm', '/home/akbajpai/PycharmProjects/pythonProject/Subprocess/DEMO/{}'.format(str)])
+        if st in p1:
+            print("-------------FOUND------------------"+st)
+            subprocess.call(['rm', '/home/akbajpai/PycharmProjects/pythonProject/Subprocess/DEMO/{}'.format(st)])
             p1 = subprocess.check_output(['ls', '/home/akbajpai/PycharmProjects/pythonProject/Subprocess/DEMO/'],
                                          encoding='utf-8')
-            if str in p1:
+            if st in p1:
                 print("You dont have the permission to delete that file")
             else:
-                print("File "+str+" deleted successfully")
+                print("File "+st+" deleted successfully")
         else:
             print("No file found")
 
@@ -30,7 +32,10 @@ obj = Test()
 l=['junk.txt','junk2.txt','junk3.txt']
 for each in l:
     obj.create(each)
-    # obj.check(each)
+
+# time.sleep(2)
 
 for each in l:
     obj.check(each)
+
+# print(str(c)+" files found and "+str(d)+" files deleted")
---
+++ Framework/test_check.py
@@ -0,0 +1,19 @@
+import pytest
+from check import Test
+
+inp_list = ['junk.txt', 'junk2.txt', 'junk3.txt']
+
+
+@pytest.fixture(params=inp_list)
+def setup(request):
+    retVal=request.param
+    print("value = {}\n".format(retVal))
+    return retVal
+
+def test_create(setup):
+    ob = Test()
+    ob.create(setup)
+
+def test_delete(setup):
+    ob = Test()
+    ob.check(setup)
