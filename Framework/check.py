db0735de7bd3553b8cbf575aaa967db7e4fec1fb
akbajpaiRH <akbajpai@redhat.com>
2023-01-16 10:23:43 +0530

    minor

--- Framework/check.py
+++ Framework/check.py
@@ -3,6 +3,12 @@ import os
 import time
 from collections import Counter
 import csv
+import base64
+
+secret = ""
+with open('/home/akbajpai/PycharmProjects/pythonProject/Subprocess/DEMO/secret.txt', 'r') as f:
+    line = f.readline()
+    secret = base64.urlsafe_b64decode(line)
 
 
 class Test:
@@ -12,33 +18,53 @@ class Test:
                 ['touch',
                  '/home/akbajpai/PycharmProjects/pythonProject/Subprocess/DEMO/{file_name}'.format(file_name=str)],
                 stdout=subprocess.PIPE, encoding='utf-8')
+            assert p1.returncode == 0
             return 1
-        except:
-            print("File with same name exits")
+        except Exception as e:
+            print(e)
 
     def check(self, st, d):
         # print(os.getcwd())
         p1 = subprocess.run(['ls', '/home/akbajpai/PycharmProjects/pythonProject/Subprocess/DEMO/'], text=True,
                             stdout=subprocess.PIPE,
                             encoding='utf-8')
+        assert p1.returncode == 0
+        # print(p1.stdout)
+        if st == 'junk.txt':
+            print("inside if")
+            # pswrd = "Akashbajpai$1226"
+            pswrd = secret
+            inp1 = subprocess.Popen(['echo', pswrd], stdout=subprocess.PIPE)
+            inp2 = subprocess.Popen(["sudo", "-S", "chattr", "+i",
+                                     "/home/akbajpai/PycharmProjects/pythonProject/Subprocess/DEMO/{file_name}".format(
+                                         file_name=st)], stdin=inp1.stdout, stdout=subprocess.PIPE)
+            out, err = inp2.communicate()
+
         if st in p1.stdout:
-            print("-------------FOUND------------------" + st)
             try:
-                subprocess.call(['rm', '/home/akbajpai/PycharmProjects/pythonProject/Subprocess/DEMO/{}'.format(st)])
-                d += 1
+                cmd = "rm -f /home/akbajpai/PycharmProjects/pythonProject/Subprocess/DEMO/{file_name}"
+                cmd = cmd.format(file_name=st)
+                print(cmd)
+                r = subprocess.run(cmd, shell=True)
+
+                if r.returncode == 0:
+                    d += 1
+
                 p1 = subprocess.run(['ls', '/home/akbajpai/PycharmProjects/pythonProject/Subprocess/DEMO/'], text=True,
                                     stdout=subprocess.PIPE,
                                     encoding='utf-8')
                 assert st not in p1.stdout
                 return d
-            except:
+            except Exception as e:
                 print("You don't have permission to delete the file")
+                print(str(e))
+                return d
         else:
             print("No file found")
 
 
 obj = Test()
-l = ['junk.txt', 'junk2.txt', 'junk3.txt','junk4.txt','junk5.txt','junk.txt','junk7.txt','junk5.txt']
+l = ['junk3.txt', 'junk2.txt', 'junk3.txt', 'junk4.txt', 'junk5.txt', 'junk8.txt', 'junk7.txt', 'junk5.txt']
 c = len(l)
 l1 = set(l)
 print(c)
@@ -52,6 +78,7 @@ print(i)
 
 d = 0
 for each in l1:
+    print(each)
     d = obj.check(each, d)
 print(d)
 
--- Subprocess/sub_call.py
+++ Subprocess/sub_call.py
@@ -1,3 +1,9 @@
 import subprocess
+from subprocess import PIPE,Popen
+# p=subprocess.call(["ls", "-l"])
+pswrd = "Akashbajpai$1226"
+p = Popen(['echo', pswrd], stdout=PIPE)
+p1 = Popen(["sudo", "-S","ls", "-la"], stdin=p.stdout, stdout=PIPE)
 
-p=subprocess.call(["ls", "-l"])
+# stdout= p1.communicate()[0]
+# print(stdout)
